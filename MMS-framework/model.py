3e22efe46a6e53d534d30c9ba05d2a6e28dad8cf
Suhas Bharadwaj <98911101+suhasbharadwajx@users.noreply.github.com>
2026-06-28 18:30:51 +0400

    Refactor PiezoInversePINN model initialization and forward pass

--- MMS-framework/model.py
+++ MMS-framework/model.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 class PiezoInversePINN(nn.Module):
     def __init__(self):
         super().__init__()
+        
         self.net = nn.Sequential(
             nn.Linear(3, 150), nn.Tanh(),
             nn.Linear(150, 150), nn.Tanh(),
@@ -14,13 +15,18 @@ class PiezoInversePINN(nn.Module):
             nn.Linear(150, 150), nn.Tanh(),
             nn.Linear(150, 3)
         )
+        
         self.e15_pred = nn.Parameter(torch.tensor([0.10], dtype=torch.float32))
+
         for m in self.modules():
             if isinstance(m, nn.Linear):
                 nn.init.xavier_uniform_(m.weight)
 
     def forward(self, x_tensor):
         x, z, t = x_tensor[:, 0:1], x_tensor[:, 1:2], x_tensor[:, 2:3]
+        
         out = self.net(x_tensor)
+        
         D_bc = x * (1 - x) * z * (1 - z) * (t**2)
+        
         return D_bc * out[:, 0:1], D_bc * out[:, 1:2], D_bc * out[:, 2:3]
